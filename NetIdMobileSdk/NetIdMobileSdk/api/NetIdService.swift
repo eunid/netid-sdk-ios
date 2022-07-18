56cb696f72645c0caaeb62f38ea70436d07c92be
Felix Hug <felix.hug@letsdev.de>
2022-07-18 10:47:46 +0200

    Created AppAuthManager and added copyright header

--- NetIdMobileSdk/NetIdMobileSdk/api/NetIdService.swift
+++ NetIdMobileSdk/NetIdMobileSdk/api/NetIdService.swift
@@ -1,6 +1,16 @@
+// Copyright 2022 European netID Foundation (https://enid.foundation)
 //
-// Created by Felix Hug on 13.07.22.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import Foundation
 
@@ -9,19 +19,36 @@ class NetIdService: NSObject {
     static let sharedInstance = NetIdService()
     private var netIdConfig: NetIdConfig?
     private var netIdListener: [NetIdServiceDelegate] = []
+    private var appAuthManager: AppAuthManager?
 
-    public func registerListener(_ listener: NetIdServiceDelegate){
+    public func registerListener(_ listener: NetIdServiceDelegate) {
         netIdListener.append(listener)
     }
 
     public func initialize(_ netIdConfig: NetIdConfig) {
         if self.netIdConfig != nil {
-            print("Configuration already been set.")
+            Logger.shared.debug("Configuration already been set.")
         } else {
             self.netIdConfig = netIdConfig
-            // TODO fetch configuration from .well-known endpoint
+            appAuthManager = AppAuthManager(delegate: self)
+            appAuthManager?.fetchConfiguration(netIdConfig.host)
         }
     }
 
-    public func authorize() {}
+    public func getNedIdConfig() -> NetIdConfig? {
+        netIdConfig
+    }
+
+    public func authorize() {
+    }
+}
+
+extension NetIdService: AppAuthManagerDelegate {
+    func didReceiveConfig() {
+        
+    }
+
+    func didReceiveError() {
+
+    }
 }
